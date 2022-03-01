e97d46234fc690fa3d7b3a4e6c2dce1dd6b08191
Shivank Kacker <kacker.shivank@gmail.com>
2022-03-01 20:19:56 +0530

    init

---
+++ inc/core.php
@@ -0,0 +1,12 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/x-icon" href="med/asset_2.png" />
+<link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+<link href="inc/lib/fontawesome/css/all.min.css" rel="stylesheet">
+<link href="css/common.css?v=<?php echo time();?>" rel="stylesheet">
+<link href="css/header.css?v=<?php echo time();?>" rel="stylesheet">
+<link href="css/checkbox.css?v=<?php echo time();?>" rel="stylesheet">
+<script src="inc/lib/jquery-3.1.1.js"></script>
+<script src="inc/lib/jquery.data-parallax.js"></script>
+<script src="inc/lib/scroller.js"></script>
+<link href="inc/lib/flags/css/flag-icon.css" rel="stylesheet">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
